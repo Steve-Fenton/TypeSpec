a36fdc9326828a4347a6562bbe888977f52a3c4b
Steve Fenton <github@stevefenton.co.uk>
2017-10-22 19:59:10 +0100

    Update Node example to wrap fs in promise.

--- TypeSpec.NodeExamples/app.ts
+++ TypeSpec.NodeExamples/app.ts
@@ -5,23 +5,34 @@ import * as fs from 'fs';
 
 AutoRunner.testReporter = new TapReporter();
 
-// Run all the specifications within the "./Specifications" folder
-fs.readdir('./Specifications', function (err, items) {
-    const specifications: string[] = [];
-    for (const item of items) {
-        specifications.push('/Specifications/' + item);
-    }
+// Example of dynamically getting all specifications from a folder
+function getSpecifications() {
+    return new Promise((resolve: (specifications: string[]) => void, reject: (error: any) => void) => {
+        fs.readdir('./Specifications', function (err, items) {
+            const specifications: string[] = [];
+            for (const item of items) {
+                specifications.push('/Specifications/' + item);
+            }
 
-    AutoRunner.run.apply(AutoRunner, specifications)
-        .then(() => {
-            // The rest of the code is just to let you "press any key to exit"
-            const rl = readline.createInterface({
-                input: process.stdin,
-                output: process.stdout
-            });
-
-            rl.question('Press any key to exit...', (answer: any) => {
-                rl.close();
-            });
+            resolve(specifications);
         });
-});
+    });
+}
+
+// Run all the specifications
+getSpecifications()
+    .then((specs) => {
+
+        AutoRunner.run.apply(AutoRunner, specs)
+            .then(() => {
+                // This is just console code, not related to testing
+                const rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+                rl.question('Press any key to exit...', (answer: any) => {
+                    rl.close();
+                });
+            });
+    });
